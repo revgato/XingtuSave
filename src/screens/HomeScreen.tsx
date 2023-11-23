7e82f6494dc5e5e24ba8aeceb1b1dcc50c6df28c
phucduongminh <phucbopbnation@gmail.com>
2023-11-24 00:18:39 +0700

    Setup: Add Fonts + BottomNav

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -24,8 +24,6 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-import TabNavigator from './src/navigators/TabNavigator';
-import { NavigationContainer } from '@react-navigation/native';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -57,7 +55,7 @@ function Section({children, title}: SectionProps): JSX.Element {
   );
 }
 
-function App(): JSX.Element {
+function HomeScreen(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -65,9 +63,36 @@ function App(): JSX.Element {
   };
 
   return (
-    <NavigationContainer>
-    <TabNavigator/>
-    </NavigationContainer>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -90,4 +115,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default HomeScreen;
